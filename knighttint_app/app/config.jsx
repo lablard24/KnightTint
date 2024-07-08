fda4b981580b7bc1f85a6112ac34345f428b1261
lablard24 <lucknerablard@gmail.com>
2024-07-08 15:40:32 -0400

    Heroku Endpoint

--- knighttint_app/app/config.jsx
+++ knighttint_app/app/config.jsx
@@ -1,8 +1,18 @@
 
 export const WEBSOCKET_IP = 'ws://192.168.0.198:81/';
-export const SERVER_PROTOCOL = 'http';
+/*export const SERVER_PROTOCOL = 'http';
 export const SERVER_IP = '10.0.0.138';
 export const SERVER_PORT = '3001';
+export const REGISTER_ENDPOINT = '/auth/register';
+export const LOGIN_ENDPOINT = '/auth/login';
+export const CHECKEMAIL_ENDPOINT = '/auth/checkEmail';
+export const RESET_PASSWORD_ENDPOINT = '/auth/resetPassword';*/
+
+export const SERVER_PROTOCOL = 'https'; 
+export const SERVER_DOMAIN = '//knighttint-0ea6e634bb48.herokuapp.com'
+export const SERVER_PORT = process.env.PORT || '3001'; 
+
+// Endpoints
 export const REGISTER_ENDPOINT = '/auth/register';
 export const LOGIN_ENDPOINT = '/auth/login';
 export const CHECKEMAIL_ENDPOINT = '/auth/checkEmail';
